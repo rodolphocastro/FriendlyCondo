0af83062e117286bc2e89437968204f7d162c8a0
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-08-04 00:26:41 -0300

    feat: Log to logcat and Crashlytics

--- app/src/main/java/com/ardc/friendlycondo/MainActivity.kt
+++ app/src/main/java/com/ardc/friendlycondo/MainActivity.kt
@@ -11,13 +11,33 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ch.qos.logback.classic.android.LogcatAppender
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.AppenderBase
 import com.ardc.friendlycondo.ui.theme.FriendlyCondoTheme
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.slf4j.Marker
-import org.slf4j.helpers.SubstituteLogger
+
+
+/**
+ * ## CrashylyticsAppender
+ * Logback appender that forwards events to Firebase Crashlytics alongside logcat.
+ */
+class LogcatCrashlyticsAppender : LogcatAppender() {
+    private var crashlyticsInstance: FirebaseCrashlytics = Firebase.crashlytics
+
+    @Override
+    override fun append(event: ILoggingEvent?) {
+        super.append(event)
+        event?.let {
+            crashlyticsInstance.log(encoder.layout.doLayout(it))
+        }
+    }
+}
 
 class MainActivity : ComponentActivity() {
     private lateinit var logger: Logger
@@ -33,7 +53,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Button(onClick = { crashWithText("Testing Crashlytics") }) {
+                    Button(onClick = { crashWithText("Testing Crashlytics", logger) }) {
                         Greeting("Android")
                     }
                 }
@@ -42,7 +62,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-private fun crashWithText(message: String): Unit {
+private fun crashWithText(message: String, logger: Logger): Unit {
+    logger.warn("Something is wrong, I can feel it!")
     throw RuntimeException(message)
 }
 
