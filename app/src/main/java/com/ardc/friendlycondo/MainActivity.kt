d4746d223f4cb42ece52452f946987d52a7e9feb
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-08-04 00:06:35 -0300

    housekeeping: Migrate to Logback-Android

--- app/src/main/java/com/ardc/friendlycondo/MainActivity.kt
+++ app/src/main/java/com/ardc/friendlycondo/MainActivity.kt
@@ -12,14 +12,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ardc.friendlycondo.ui.theme.FriendlyCondoTheme
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.slf4j.Marker
+import org.slf4j.helpers.SubstituteLogger
 
 class MainActivity : ComponentActivity() {
     private lateinit var logger: Logger
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        logger = LoggerFactory.getLogger("org.slf4j:slf4j-android")
+        logger = LoggerFactory.getLogger(MainActivity::class.java)
+        logger.info("Hello, world")
         super.onCreate(savedInstanceState)
         setContent {
             FriendlyCondoTheme {
