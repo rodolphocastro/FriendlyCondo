c704b1277f21ef1423e1c727721c05f641c2235e
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-08-04 23:39:54 -0300

    housekeeping: Center text, add spacing

--- app/src/main/java/com/ardc/friendlycondo/MainActivity.kt
+++ app/src/main/java/com/ardc/friendlycondo/MainActivity.kt
@@ -5,16 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ardc.friendlycondo.ui.theme.FriendlyCondoTheme
@@ -69,11 +68,19 @@ fun Greeting(log: Logger? = null) {
             contentDescription = "People standing around with packages"
         )
 
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(32.dp)
+        )
+
         Text(
             text = "Welcome to FriendlyCondo!",
             modifier = Modifier
                 .padding(5.dp, 0.dp)
-                .align(Alignment.CenterHorizontally),
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
         )
     }
 }
