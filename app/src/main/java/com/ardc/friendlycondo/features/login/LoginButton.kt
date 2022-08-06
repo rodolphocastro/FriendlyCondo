5329897011c8663f994f0a8c1a0b0ef5f5413bca
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-08-06 17:01:07 -0300

    feat: Change Icons based on Login's State

--- app/src/main/java/com/ardc/friendlycondo/features/login/LoginButton.kt
+++ app/src/main/java/com/ardc/friendlycondo/features/login/LoginButton.kt
@@ -2,6 +2,7 @@ package com.ardc.friendlycondo.features.login
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
@@ -12,27 +13,48 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ardc.friendlycondo.AppCanvas
 
+/**
+ * ## LoginButton
+ * Allows an user to log into the application.
+ * @param state current state of login within the App
+ * @param onButtonClicked delegate to execute when the button is clicked
+ */
 @Composable
-fun LoginButton(state: LoginState, onButtonClicked: () -> Unit) {
+fun LoginButton(
+    state: LoginState,
+    onButtonClicked: () -> Unit
+) {
     Button(
         onClick = onButtonClicked,
         modifier = Modifier
             .fillMaxWidth()
             .height(32.dp)
     ) {
-        Icon(Icons.Filled.Person, contentDescription = "A person")
+        if (state.isLoggedIn)
+            Icon(Icons.Filled.ArrowForward, contentDescription = "A person")
+        else Icon(
+            Icons.Filled.Person,
+            contentDescription = "A person"
+        )
+
         Spacer(Modifier.width(16.dp))
-        if (state.isLoggedIn) Text(text = "Continue") else Text(text = "Login")
+
+        if (state.isLoggedIn)
+            Text(text = "Continue")
+        else Text(text = "Login")
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewLoggedOffButton(): Unit {
+fun PreviewButtons() {
     AppCanvas {
-        Column {
+        Column(verticalArrangement = Arrangement.SpaceEvenly) {
             LoginButton(state = LoginState()) {
-                /* do Nothing */
+                /* do nothing */
+            }
+            LoginButton(state = LoginState("anuser")) {
+                /* do nothing */
             }
         }
     }
